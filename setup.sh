0a26aa7ad95d35452df4aaee6ff60c861b1c6868
Robert Hilton <robert.a.hilton.jr@gmail.com>
2022-01-23 16:49:31 -0500

    Setup script for dev environment

---
+++ setup.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+VSCODE_x86_64=https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64
+VSCODE_aarch64=https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-arm64
+VSCODE=
+case "$(arch)" in
+    "x86_64"|"amd64")
+        VSCODE=${VSCODE_x86_64}
+        ;;
+    "aarch64"|"arm64")
+        VSCODE=${VSCODE_aarch64}
+        ;;
+    *)
+        errmsg "Invalid architecture \"$1\" supported architectures are: x86_64 amd64 aarch64 arm64"
+        exit
+        ;;
+esac
+
+function pause(){
+    read -s -n 1 -p "Press any key to continue . . ."
+    echo ""
+}
+echo "Enter a name for this computer:"
+read CKHOST
+
+#Get username if user is sudoing
+USERNAME=$(who | awk '{print $1}')
+echo "Running setup for user ${USERNAME}"
+sudo nmcli general hostname ${CKHOST}
+
+if [[ $EUID -ne 0 ]] || [ "${USERNAME}" = "root" ]; then
+    echo "This script must be run with sudo from the user account you plan on using" 
+    exit 1
+fi
+
+sudo apt-get update && apt-get upgrade -y && apt-get install -y docker.io wget curl gparted
+
+sudo usermod -aG docker $USERNAME
+newgrp docker
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
+docker pull guitar24t/ck-ros:latest
+
+#Disable password entry for sudo
+echo "$USERNAME ALL=(ALL) NOPASSWD:ALL" | (sudo su -c 'EDITOR="tee -a" visudo')
+
+cd ~
+curl -sLo vscode.deb "${VSCODE}"
+sudo dpkg --install vscode.deb
+sudo apt-get install -f -y
+sudo dpkg --install vscode.deb
+
+mkdir repos
+cd repos
+ssh-keygen -b 2048 -t rsa -q -N ""
+cat ~/.ssh/id_rsa.pub
+echo "Please add this key to github named ${CKHOST} before continuing!"
+pause
+git clone https://github.com/frcteam195/ros_dev.git
+
+sudo reboot
